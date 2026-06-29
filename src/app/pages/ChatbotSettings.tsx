6f3025dedaf4f284e95d4154dae092b866f8b3f1
Ganesh thakur <ganesh.thakur@onsumaye.com>
2026-06-29 16:21:45 +0530

    general and appearnce tab page design fixing

--- src/app/pages/ChatbotSettings.tsx
+++ src/app/pages/ChatbotSettings.tsx
@@ -250,7 +250,13 @@ export function ChatbotSettings() {
         disabled={disabled}
         className="sr-only peer"
       />
-      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
+      <div
+        className={`w-11 h-6 rounded-full peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 ${
+          checked
+            ? 'bg-blue-600 after:translate-x-full after:border-white'
+            : 'bg-gray-200 dark:bg-gray-700'
+        }`}
+      />
     </label>
   );
 
